fc830423019a1df26205b7e289b2df05f50d6a33
Adrien LUCAS <adrien.lucas@smile.fr>
2017-11-02 14:44:22 +0100

    Add HttpFoundation

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+require('vendor/autoload.php');
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$response = new Response();
+
 // Routing and main controller
 $_pages = [
     '' => 'home',
@@ -8,34 +17,38 @@ $_pages = [
     '/exit' => 'redirect_to_google',
 ];
 
-$request = $_SERVER['PATH_INFO'];
-if(array_key_exists($request, $_pages)) {
-    $controller = sprintf('acme_page_%s', $_pages[$request]);
-    echo "<html><body>";
-    $controller();
-    echo "</body></html>";
+$requestedPage = $request->getPathInfo();
+if(array_key_exists($requestedPage, $_pages)) {
+    $controller = sprintf('acme_page_%s', $_pages[$requestedPage]);
+    $controllerResponse = $controller($request);
+    if(is_a($controllerResponse, Response::class)) {
+        $response = $controllerResponse;
+    } else {
+        $response->setContent(sprintf('<html><body>%s</body></html>', $controllerResponse));
+    }
 } else {
-    http_response_code(404);
-    printf('Page "%s" not found.', $request);
+    $response->setStatusCode(Response::HTTP_NOT_FOUND);
+    $response->setContent(sprintf('Page "%s" not found.', $requestedPage));
 }
+
+$response->send();
 
 // Pages controllers
 function acme_page_home() {
-    echo 'Welcome to ACME official website !<br/>';
-    echo '<ul><li><a href="/hello">Come and let us greet you !</a></li>'.
-        '<li><a href="/exit">Exit this website</a></li></ul>';
+    return 'Welcome to ACME official website !<br/>'
+        .'<ul><li><a href="/hello">Come and let us greet you !</a></li>'
+        .'<li><a href="/exit">Exit this website</a></li></ul>';
 }
 
-function acme_page_hello_name() {
-    $name = isset($_GET['name']) ? $_GET['name'] : false;
+function acme_page_hello_name(Request $request) {
+    $name = $request->get('name', false);
     if(empty($name)) {
-        echo 'Tell us your name : <form><input name="name" /><form>';
+        return 'Tell us your name : <form><input name="name" /><form>';
     } else {
-        printf('Hi %s ! Nice to see you !', $name);
+        return sprintf('Hi %s ! Nice to see you !', $name);
     }
 }
 
 function acme_page_redirect_to_google() {
-    http_response_code(302);
-    header('Location: http://google.fr');
+    return new RedirectResponse('http://smile.eu');
 }
